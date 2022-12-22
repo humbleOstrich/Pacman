4cf5d74bd458418012fc7502f8d8717204102dc5
humbleOstrich <118448454+humbleOstrich@users.noreply.github.com>
2022-12-22 22:32:47 +0500

    Fix camera bug, change floor/ background structure

--- test/pygame_project_dev.py
+++ test/pygame_project_dev.py
@@ -57,16 +57,13 @@ def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#' or level[y][x] == '/':
+            if level[y][x] == '#' or level[y][x] == '/':
                 Tile('wall', x, y)
             elif level[y][x] == '@':
-                Tile('empty', x, y)
+
                 new_player = Player(x, y)
     map_width = 50 * len(level[0])
     map_height = 50 * len(level)
-    # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
 
@@ -77,24 +74,46 @@ def terminate():
 
 def load_level(filename):
     filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
 tile_images = {
-    'wall': load_image('wall.png'),
-    'empty': load_image('floor.png')
+    'wall': load_image('wall.png')
 }
 player_image = load_image('player.png', colorkey=-1)  # добавить анимацию игрока
 
 tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.type = str(tile_type)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def move(self, direction):
+
+        if direction == "right":
+            self.rect = self.rect.move(10, 0)
+        elif direction == "left":
+            self.rect = self.rect.move(-10, 0)
+        elif direction == "down":
+            self.rect = self.rect.move(0, 10)
+        elif direction == "up":
+            self.rect = self.rect.move(0, -10)
 
 
 class Camera:
@@ -112,42 +131,6 @@ class Camera:
     def update(self, target):
         self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
         self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
-        # if a + width > map_width:
-        #     if 0 < a + width:
-        #         self.dx = a
-        # if b + height > map_height:
-        #     if 0 < b + height:
-        #         self.dy = b
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.type = str(tile_type)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-        self.mask = pygame.mask.from_surface(self.image)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.mask = pygame.mask.from_surface(self.image)
-
-    def move(self, direction):
-
-        if direction == "right":
-            self.rect = self.rect.move(10, 0)
-        elif direction == "left":
-            self.rect = self.rect.move(-10, 0)
-        elif direction == "down":
-            self.rect = self.rect.move(0, 10)
-        elif direction == "up":
-            self.rect = self.rect.move(0, -10)
 
 
 pygame.display.set_caption('pacman')
@@ -159,9 +142,9 @@ player_group = pygame.sprite.Group()
 FPS = 50
 clock = pygame.time.Clock()
 start_screen()
-player, level_x, level_y = generate_level(load_level('level_1.txt'))
-text_map = load_level('level_1.txt')
-
+map_name = "level_1.txt"
+player, level_x, level_y = generate_level(load_level(map_name))
+text_map = load_level(map_name)
 camera = Camera()
 while True:
     for event in pygame.event.get():
@@ -177,12 +160,12 @@ while True:
                 player.move("up")
             elif event.key == pygame.K_DOWN:
                 player.move("down")
-
-    all_sprites.draw(screen)
-    tiles_group.draw(screen)
-    player_group.draw(screen)
     camera.update(player)
     for sprite in all_sprites:
         camera.apply(sprite)
+    screen.fill((255, 228, 181))
+    all_sprites.draw(screen)
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
