86f6b0414288a06199b3a166f3613e81a8766aa3
humbleOstrich <118448454+humbleOstrich@users.noreply.github.com>
2022-12-26 19:16:23 +0500

    Added collision, code adjusted to fit maze

--- pygame_project_dev.py
+++ pygame_project_dev.py
@@ -4,7 +4,7 @@ import pygame
 pygame.font.init()
 
 
-size = width, height = 500, 500
+size = width, height = 1400, 700
 WIDTH, HEIGHT = width, height
 screen = pygame.display.set_mode(size)
 map_width, map_height = 0, 0
@@ -27,13 +27,11 @@ def load_image(name, colorkey=None):
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))  # добавить заставку
+    intro_text = ["Здесь", "",
+                  "Будет заставка"]
+    fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))  # добавить заставку
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
+    font = pygame.font.Font(None, 50)
     text_coord = 50
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('white'))
@@ -59,16 +57,12 @@ def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#' or level[y][x] == '/':
-                Tile('wall', x, y)
+            if level[y][x] == 'x':
+                platforms.append(Platform((x, y)))
             elif level[y][x] == '@':
-                Tile('empty', x, y)
                 new_player = Player(x, y)
     map_width = 50 * len(level[0])
     map_height = 50 * len(level)
-    # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
 
@@ -79,57 +73,25 @@ def terminate():
 
 def load_level(filename):
     filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-tile_images = {
-    'wall': load_image('wall.png'),
-    'empty': load_image('floor.png')
-}
-player_image = load_image('player.png', colorkey=-1)  # добавить анимацию игрока
-
-tile_width = tile_height = 50
+player_image = load_image('pacman.png', colorkey=-1)  # добавить анимацию игрока
 
 
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
+class Platform(pygame.sprite.Sprite):
 
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
+    def __init__(self, coords):
+        super().__init__(all_sprites)
+        self.rect = pygame.Rect(coords[0] * 25, coords[1] * 25, 25, 25)
+        self.rect.x = coords[0] * 25
+        self.rect.y = coords[1] * 25
 
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
-        # if a + width > map_width:
-        #     if 0 < a + width:
-        #         self.dx = a
-        # if b + height > map_height:
-        #     if 0 < b + height:
-        #         self.dy = b
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.type = str(tile_type)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-        self.mask = pygame.mask.from_surface(self.image)
+    def update(self):
+        pygame.draw.rect(screen, pygame.Color("black"), (self.rect.x, self.rect.y, 25, 25))
 
 
 class Player(pygame.sprite.Sprite):
@@ -137,8 +99,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.mask = pygame.mask.from_surface(self.image)
+            25 * pos_x, 25 * pos_y)
 
     def move(self, direction):
 
@@ -152,39 +113,71 @@ class Player(pygame.sprite.Sprite):
             self.rect = self.rect.move(0, -10)
 
 
+def draw_rect(x, y, w, h):
+    pygame.draw.rect(screen, pygame.Color('black'), (x, y, w, h))
+
+
 pygame.display.set_caption('pacman')
 
 player = None
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+platforms = []
 FPS = 50
+speed = 10
 clock = pygame.time.Clock()
 start_screen()
-player, level_x, level_y = generate_level(load_level('level_1.txt'))
-text_map = load_level('level_1.txt')
-
-camera = Camera()
+map_name = "level_7.txt"
+player, level_x, level_y = generate_level(load_level(map_name))
+text_map = load_level(map_name)
 while True:
+    move_up = True
+    move_down = True
+    move_left = True
+    move_right = True
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                player.move("right")
-            elif event.key == pygame.K_LEFT:
-                player.move("left")
-            elif event.key == pygame.K_UP:
-                player.move("up")
-            elif event.key == pygame.K_DOWN:
-                player.move("down")
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                char = pygame.Rect(player.rect.x + speed, player.rect.y, 40, 40)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_right = False
+                        break
+                if move_right:
+                    player.move("right")
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                char = pygame.Rect(player.rect.x - speed, player.rect.y, 40, 40)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_left = False
+                        break
+                if move_left:
+                    player.move("left")
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                char = pygame.Rect(player.rect.x, player.rect.y - speed, 40, 40)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_up = False
+                        break
+                if move_up:
+                    player.move("up")
 
-    all_sprites.draw(screen)
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                char = pygame.Rect(player.rect.x, player.rect.y + speed, 40, 40)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_down = False
+                        break
+                if move_down:
+                    player.move("down")
+    all_sprites.update()
     tiles_group.draw(screen)
     player_group.draw(screen)
-    camera.update(player)
-    for sprite in all_sprites:
-        camera.apply(sprite)
     pygame.display.flip()
+    screen.fill((255, 228, 181))
     clock.tick(FPS)
