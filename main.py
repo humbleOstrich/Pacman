91281dd4ae52cb37a3a4ea9853f6c703b957e019
Kote5615 <miss.vestik@yandex.ru>
2022-12-30 11:26:28 +0300

    добавлена запись в базу данных, появились начальны

--- main.py
+++ main.py
@@ -1,79 +1,71 @@
-import pygame
+import os
 import sys
+import pygame
+import random
+import sqlite3
 import button
 
 pygame.init()
 
-# window size
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 500
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+size = width, height = 1500, 700
+WIDTH, HEIGHT = width, height
+screen = pygame.display.set_mode(size)
+map_width, map_height = 0, 0
 
-# define game variables
-tile_size = 25
-gameover = False
-speed = 10
-# player = None
-# fps
-FPS = 60
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
-# load images
-dead_img = pygame.image.load('data/img/player/88bdc0d7bc024ec6a225b1bd841a69ecly65tYX0FyZMF0I4-20.png').convert_alpha()
-player_img = pygame.image.load('data/img/player/88bdc0d7bc024ec6a225b1bd841a69ecly65tYX0FyZMF0I4-0.png').convert_alpha()
 start_img = pygame.image.load('data/img/btns/Play Button.png').convert_alpha()
 exit_img = pygame.image.load('data/img/btns/Quit Button.png').convert_alpha()
 info_img = pygame.image.load('data/img/btns/Info Square Button.png').convert_alpha()
 resume_img = pygame.image.load('data/img/btns/Resume Button.png').convert_alpha()
 menu_img = pygame.image.load('data/img/btns/Menu Button.png').convert_alpha()
 home_img = pygame.image.load('data/img/btns/Home Square Button.png').convert_alpha()
-tile_images = {
-    'wall': pygame.image.load('data/img/walls/walls/image_part_001.png'),
-    'empty': pygame.image.load('data/img/floor.png')
-}
-background = (255, 228, 181)
 death_background = (255, 69, 0)
-player_img = pygame.transform.scale(player_img, (25, 25))
-coords_for_first_btn = (150, 80)
-coords_for_second_btn = (150, 200)
-coords_for_third_btn = (150, 320)
+
+coords_for_first_btn = ((width // 2) - 150, 80)
+coords_for_second_btn = ((width // 2) - 150, 200)
+coords_for_third_btn = ((width // 2) - 150, 320)
 
 
-# func
-def exit():
-    pygame.quit()
-    sys.exit()
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    if colorkey is not None:
+        # image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
-def draw_grid():
-    for line in range(0, 50):
-        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, SCREEN_HEIGHT))
-
-
-# func
 def start_screen():
-    fon = pygame.transform.scale(pygame.image.load('data/img/floor.png'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+    fon = pygame.transform.scale(pygame.image.load('data/img/floor.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     while True:
         # create btns
         start_button = button.Button(coords_for_first_btn[0], coords_for_first_btn[1], start_img, 0.35)
         resume_button = button.Button(coords_for_second_btn[0], coords_for_second_btn[1], resume_img, 0.35)
-        info_button = button.Button(410, 40, info_img, 0.3)
+        info_button = button.Button(width // 2 + 200, 40, info_img, 0.3)
         exit_button = button.Button(coords_for_third_btn[0], coords_for_third_btn[1], exit_img, 0.35)
         if start_button.draw(screen):
+            global gameover
+            gameover = False
+            global counter
+            counter = 0
+
             return
         if exit_button.draw(screen):
-            exit()
+            terminate()
         if info_button.draw(screen):
             print('здесь будет окно с правилами')
         if resume_button.draw(screen):
             print('здесь будет окно с результатами')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                terminate()
         pygame.display.flip()
         pygame.time.delay(FPS)
 
@@ -83,13 +75,36 @@ def end_screen():
         screen.fill(death_background)
         start_button = button.Button(coords_for_first_btn[0], coords_for_first_btn[1], start_img, 0.35)
         resume_button = button.Button(coords_for_second_btn[0], coords_for_second_btn[1], resume_img, 0.35)
-        info_button = button.Button(410, 40, info_img, 0.3)
-        home_button = button.Button(410, 120, home_img, 0.3)
+        info_button = button.Button(width // 2 + 200, 40, info_img, 0.3)
+        home_button = button.Button(width // 2 + 200, 120, home_img, 0.3)
         exit_button = button.Button(coords_for_third_btn[0], coords_for_third_btn[1], exit_img, 0.35)
         if start_button.draw(screen):
+            # gameover = False
+            global gameover
+            gameover = False
+            global counter
+            map_name = f"levels/level_{level}.txt"
+            global player
+            global level_x
+            global level_y
+            # all_sprites.kill()
+            for player in player_group:
+                player.kill()
+                del player
+            for point in point_group:
+                point.kill()
+                del point
+            for enemy in enemy_group:
+                enemy.kill()
+                del enemy
+
+            player, level_x, level_y = generate_level(load_level(map_name))
+            counter = 0
+            screen.fill((255, 228, 181))
+
             return
         if exit_button.draw(screen):
-            exit()
+            terminate()
         if info_button.draw(screen):
             print('здесь будет окно с правилами')
         if resume_button.draw(screen):
@@ -98,98 +113,271 @@ def end_screen():
             start_screen()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                terminate()
         pygame.display.flip()
         pygame.time.delay(FPS)
 
 
-hero = None
+def generate_level(level):
+    global map_width, map_height
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == 'x':
+                platforms.append(Platform((x, y)))
+            elif level[y][x] == '@':
+                new_player = Player(x, y)
+            elif level[y][x] == 'p':
+                points.append(Point((x, y)))
+            elif level[y][x] == 'e':
+                enemies.append(Enemy(x, y, enemy_e_image))
+            elif level[y][x] == 'n':
+                enemies.append(Enemy(x, y, enemy_n_image))
+            elif level[y][x] == 'm':
+                enemies.append(Enemy(x, y, enemy_m_image))
+            elif level[y][x] == 'y':
+                enemies.append(Enemy(x, y, enemy_y_image))
+    map_width = 50 * len(level[0])
+    map_height = 50 * len(level)
+    return new_player, x, y
 
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.type = str(tile_type)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_size * pos_x, tile_size * pos_y)
-        self.mask = pygame.mask.from_surface(self.image)
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def add_to_database(level, score):
+    con = sqlite3.connect("results_db.sqlite")
+    cur = con.cursor()
+    cur.execute(f'INSERT INTO results(level,score) VALUES({level}, {score})').fetchall()
+    con.commit()
+    con.close()
 
 
 def load_level(filename):
-    filename = "data/levels/" + filename
-    # читаем уровень, убирая символы перевода строки
+    filename = "data/" + filename
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-    # world = World(world_data)
 
 
-class World():
-    def __init__(self, data):
-        self.tile_list = []
-
-        # load images
-        dirt_img = pygame.image.load('data/img/walls/walls/image_part_001.png')
-        grass_img = pygame.image.load('data/img/walls/walls/block.png')
-
-        row_count = 0
-        for row in data:
-            col_count = 0
-            for stri in row:
-                print(stri, 'str')
-                if stri == '@':
-                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                if stri == '.':
-                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                col_count += 1
-            row_count += 1
-
-        # print(self.tile_list)
+player_image = load_image('img/pacman.png')  # добавить анимацию игрока
+enemy_e_image = load_image('img/ghost.png')
+enemy_n_image = load_image('img/ghost_2.png')
+enemy_m_image = load_image('img/ghost_3.png')
+enemy_y_image = load_image('img/ghost_4.png')
 
 
-    def draw(self):
-        for tile in self.tile_list:
-            screen.blit(tile[0], tile[1])
+class Platform(pygame.sprite.Sprite):
+
+    def __init__(self, coords):
+        super().__init__(all_sprites)
+        self.rect = pygame.Rect(coords[0] * 25, coords[1] * 25, 25, 25)
+        self.rect.x = coords[0] * 25
+        self.rect.y = coords[1] * 25
+
+    def update(self):
+        pygame.draw.rect(screen, pygame.Color("black"), (self.rect.x, self.rect.y, 25, 25))
 
 
+class Point(pygame.sprite.Sprite):
+
+    def __init__(self, coords):
+        super().__init__(all_sprites, point_group)
+        self.rect = pygame.Rect(coords[0] * 25, coords[1] * 25, 16, 16)
+        self.rect.x = coords[0] * 25
+        self.rect.y = coords[1] * 25
+
+    def update(self):
+        pygame.draw.circle(screen, pygame.Color("black"), (self.rect.x + 18, self.rect.y + 18), 8)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            25 * pos_x, 25 * pos_y)
+
+    def move(self, direction):
+
+        if direction == "right":
+            self.rect = self.rect.move(10, 0)
+        elif direction == "left":
+            self.rect = self.rect.move(-10, 0)
+        elif direction == "down":
+            self.rect = self.rect.move(0, 10)
+        elif direction == "up":
+            self.rect = self.rect.move(0, -10)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, name):
+        super().__init__(enemy_group, all_sprites)
+        self.image = name
+        self.rect = self.image.get_rect().move(
+            25 * pos_x, 25 * pos_y)
+        self.route = 'right'
+
+    def move(self, direction):
+        if direction == "right":
+            self.rect = self.rect.move(10, 0)
+        elif direction == "left":
+            self.rect = self.rect.move(-10, 0)
+        elif direction == "down":
+            self.rect = self.rect.move(0, 10)
+        elif direction == "up":
+            self.rect = self.rect.move(0, -10)
+
+
+pygame.display.set_caption('pacman')
+
+# player = None
+gameover = False
+all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+point_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+platforms = []
+points = []
+enemies = []
+FPS = 60
+player_speed = 10
+enemy_speed = 10
+clock = pygame.time.Clock()
 start_screen()
-run = True
-# hero = None
+level = 8
+once = 0
+map_name = f"levels/level_{level}.txt"
+player, level_x, level_y = generate_level(load_level(map_name))
+text_map = load_level(map_name)
+counter = 0
+total_points = 1000
+routes = ['right', 'left', 'down', 'up']
+coords = [(1000, 250), (1025, 250), (975, 250)]
 
-world = World(load_level('lvl1.txt'))
-print(load_level('lvl1.txt'))
-# player, level_x, level_y = generate_level(load_level('lvl1.txt'))
-while run:
-    screen.fill(background)
+while True:
+    # print(gameover)
+    move_up = True
+    move_down = True
+    move_left = True
+    move_right = True
 
-    # generate_level('data/')
-    draw_grid()
-    # hero = Player(0, 0)
-    # events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    world.draw()
-    all_sprites.draw(screen)
-    tiles_group.draw(screen)
+    if gameover is False:
+        once = 0
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.QUIT:
+                terminate()
+
+            if keys[pygame.K_d]:
+                char = pygame.Rect(player.rect.x + player_speed, player.rect.y, 60, 60)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_right = False
+                        break
+                for enemy in enemies:
+                    if char.colliderect(enemy):
+                        gameover = True
+                if move_right:
+                    player.move("right")
+            if keys[pygame.K_a]:
+                char = pygame.Rect(player.rect.x - player_speed, player.rect.y, 60, 60)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_left = False
+                        break
+                for enemy in enemies:
+                    if char.colliderect(enemy):
+                        gameover = True
+                if move_left:
+                    player.move("left")
+            if keys[pygame.K_w]:
+                char = pygame.Rect(player.rect.x, player.rect.y - player_speed, 60, 60)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_up = False
+                        break
+                for enemy in enemies:
+                    if char.colliderect(enemy):
+                        gameover = True
+                if move_up:
+                    player.move("up")
+            if keys[pygame.K_s]:
+                char = pygame.Rect(player.rect.x, player.rect.y + player_speed, 60, 60)
+                for m in platforms:
+                    if char.colliderect(m):
+                        move_down = False
+                        break
+                for enemy in enemies:
+                    if char.colliderect(enemy):
+                        gameover = True
+                if move_down:
+                    player.move("down")
+        if pygame.sprite.spritecollide(player, point_group, True):
+            counter += 1
+
+        if counter == total_points:
+            gameover = True
+            # level += 1
+
+        for enemy in enemies:
+            if int(enemy.rect.x) + 35 in range(1015, 1035) and int(enemy.rect.y) + 35 in range(250, 270):
+                possible_route = routes[random.randint(0, 3)]
+                print(enemy.route)
+                while possible_route == enemy.route:
+                    possible_route = routes[random.randint(0, 3)]
+                enemy.route = possible_route
+                print(enemy.route)
+
+            if enemy.route == 'right':
+                mob = pygame.Rect(enemy.rect.x + enemy_speed, enemy.rect.y, 70, 70)
+                for m in platforms:
+                    if mob.colliderect(m):
+                        enemy.route = routes[random.randint(0, 3)]
+                        break
+                else:
+                    enemy.move('right')
+            if enemy.route == 'left':
+                mob = pygame.Rect(enemy.rect.x - enemy_speed, enemy.rect.y, 70, 70)
+                for m in platforms:
+                    if mob.colliderect(m):
+                        enemy.route = routes[random.randint(0, 3)]
+                        break
+                else:
+                    enemy.move('left')
+            if enemy.route == 'down':
+                mob = pygame.Rect(enemy.rect.x, enemy.rect.y + enemy_speed, 70, 70)
+                for m in platforms:
+                    if mob.colliderect(m):
+                        enemy.route = routes[random.randint(0, 3)]
+                        break
+                else:
+                    enemy.move('down')
+            if enemy.route == 'up':
+                mob = pygame.Rect(enemy.rect.x, enemy.rect.y - enemy_speed, 70, 70)
+                for m in platforms:
+                    if mob.colliderect(m):
+                        enemy.route = routes[random.randint(0, 3)]
+                        break
+                else:
+                    enemy.move('up')
+
+    elif gameover is True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        if once == 0:
+            add_to_database(level, counter)
+            once += 1
+            end_screen()
+
+    all_sprites.update()
+    pygame.draw.rect(screen, (255, 0, 0), (1015, 250, 10, 10))
     player_group.draw(screen)
+    enemy_group.draw(screen)
     pygame.display.flip()
-    pygame.display.update()
-
-pygame.quit()
+    screen.fill((255, 228, 181))
+    clock.tick(FPS)
